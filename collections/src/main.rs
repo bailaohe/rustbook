0fde309b9a51118c09dfbb194753aa03327077d8
baihe <baihe@xiaomai5.com>
2017-06-09 23:03:10 +0800

    updated

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let v = vec![1, 2, 3];
 
@@ -9,5 +11,42 @@ fn main() {
 
     s.push_str("bar");
 
-    s.push('1')
+    s.push('1');
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{}-{}-{}", s1, s2, s3);
+
+    println!("s: {}", s);
+    println!("s1: {}", s1);
+
+    for c in "नमस्ते".chars() {
+        println!("{}", c);
+    }
+
+    for b in "नमस्ते".bytes() {
+        println!("{}", b);
+    }
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+    println!("{:?}", scores);
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    println!("{:?}", scores);
+
+    // let score = scores.get(&String::from("Blue"));
+    // println!("{:?}", score);
+
+    scores.insert(String::from("Blue"), 25);
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Green")).or_insert(50);
+    println!("{:?}", scores);
 }
