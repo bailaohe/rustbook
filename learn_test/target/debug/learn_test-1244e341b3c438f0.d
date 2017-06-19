358695e89c164c6d618becb5579293656c869745
baihe <baihe@xiaomai5.com>
2017-06-19 21:19:16 +0800

    updated

---
+++ learn_test/target/debug/learn_test-1244e341b3c438f0.d
@@ -0,0 +1,1 @@
+/Users/baihe/project/rustbook/learn_test/target/debug/learn_test-1244e341b3c438f0: /Users/baihe/project/rustbook/learn_test/src/lib.rs
---
+++ learn_test/target/debug/liblearn_test.d
@@ -0,0 +1,1 @@
+/Users/baihe/project/rustbook/learn_test/target/debug/liblearn_test.rlib: /Users/baihe/project/rustbook/learn_test/src/lib.rs
