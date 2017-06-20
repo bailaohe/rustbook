b95cf61e635bb549e4198fd5e82beb956bbff340
baihe <baihe@xiaomai5.com>
2017-06-20 21:25:19 +0800

    updated

--- greprs/src/lib.rs
+++ greprs/src/lib.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
+use std::env;
 
 pub fn run(config: Config) -> Result<(), Box<Error>> {
     let mut f = File::open(config.filename)?;
@@ -8,7 +9,15 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    println!("With text:\n{}", contents);
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
 
     Ok(())
 }
@@ -16,6 +25,7 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -27,8 +37,72 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
         Ok(Config {
-            query, filename
+            query: query,
+            filename: filename,
+            case_sensitive: case_sensitive,
         })
+    }
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+         let query = "duct";
+         let contents = "\
+Rust:
+safe, fast, productive.
+Pic three.
+Duct tape.";
+
+         assert_eq!(
+             vec!["safe, fast, productive."],
+             search(query, contents)
+         );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+         assert_eq!(
+             vec!["Rust:", "Trust me."],
+             search_case_insensitive(query, contents)
+         );
     }
 }
--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -2,22 +2,26 @@ extern crate greprs;
 
 use std::env;
 use std::process;
+use std::io::prelude::*;
 
 use greprs::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let mut stderr = std::io::stderr();
     
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        writeln!(
+            &mut stderr,
+            "Problem parsing arguments: {}",
+            err).expect("Could not write to stderr");
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
     if let Err(e) = greprs::run(config) {
-        println!("Application error: {}", e);
+        writeln!(
+            &mut stderr,
+            "Application error: {}", e).expect("Could not write to stderr");
 
         process::exit(1);
     }
