5c1de253764e0722b8d576dfca28cb4746a010e7
baihe <baihe@xiaomai5.com>
2017-06-20 22:15:35 +0800

    updated

--- functional/src/main.rs
+++ functional/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::thread;
+use std::time::Duration;
+
+fn simulated_expensive_calculation(intensity: i32) -> i32 {
+    println!("calculating slowly ...");
+    thread::sleep(Duration::from_secs(2))
+    intensity
 }
+
+
