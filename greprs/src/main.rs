b4154d13895f4a29acd25934b1c000781d3cfcbf
baihe <baihe@xiaomai5.com>
2017-06-21 22:58:30 +0800

    updated

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -7,10 +7,9 @@ use std::io::prelude::*;
 use greprs::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
     let mut stderr = std::io::stderr();
     
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         writeln!(
             &mut stderr,
             "Problem parsing arguments: {}",
