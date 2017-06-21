115e9f0021062d58e78135f79059c28c0baf26de
baihe <baihe@xiaomai5.com>
2017-06-21 22:27:49 +0800

    updated

--- functional/src/main.rs
+++ functional/src/main.rs
@@ -1,10 +1,37 @@
 use std::thread;
 use std::time::Duration;
 
-fn simulated_expensive_calculation(intensity: i32) -> i32 {
-    println!("calculating slowly ...");
-    thread::sleep(Duration::from_secs(2))
-    intensity
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(simulated_user_specified_value, simulated_random_number);
 }
 
+fn generate_workout(intensity: i32, random_number: i32) {
+    let expensive_closure = |num| {
+        println!("calculating slowly ...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
 
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            expensive_closure(intensity)
+        );
+        println!(
+            "Next, do {} situps!",
+            expensive_closure(intensity)
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_closure(intensity)
+            )
+        }
+    }
+}
